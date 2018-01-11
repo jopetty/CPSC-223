1b2b6e2c433556bfddf592eaa8a399a9b7fb58a8
Jackson Petty <jacksonopetty@gmail.com>
2018-01-10 17:06:29 -0700

    Add Assignment 1

---
+++ Assignment 1/encode.c
@@ -0,0 +1,84 @@
+/* 
+ * File:	encode.c
+ * Author:	Jackson Petty <jackson.petty@yale.edu>
+ * Date:	9 January 2018
+ *
+ * Description:	This file implements all code required for Assignment 1 of CPSC 223b.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define SHORT_ENDING	"an"
+#define LONG_ENDING	"o"
+
+int next_char(void)
+{
+	/*
+	 * Function: next_char()
+	 * -----------------------------------------
+	 * 	Returns the next character in the stream
+	 */
+
+	int c = getchar();
+
+	if (c != EOF) { ungetc(c,stdin); }
+
+	return c;
+}
+
+void parse_input(void)
+{
+	/*
+	 * Function parse_input()
+	 * ------------------------------------------
+	 *  	Prints out all non-letter characters as is,
+	 *  	and prints out processed versions of words.
+	 *
+	 *  	Rules:
+	 *  	 1. Move the first letter to the end.
+	 *  	 2. Add "an" to ends of words of 3 letters
+	 *  	    or less, or "o" to words of > 3 letters.
+	 *  	 3. Make the resulting word match the case
+	 *  	    of the initial word.
+	 */
+
+	unsigned int count = 0;
+	int first_char = 0;
+	int c;
+	int is_lowercase;
+
+	while (next_char() != EOF) {
+		c = getchar();
+
+		if (isalpha(c)) {
+			if (count) {
+				if (count == 1) {
+					is_lowercase ? putchar(tolower(c)) : putchar(toupper(c));
+				} else {
+					putchar(c);
+				}
+			} else {
+				is_lowercase = islower(c);
+				first_char = c;
+			}
+		} else {
+			if (first_char) {
+				(count == 1) ? putchar(first_char) 	 : putchar(tolower(first_char));
+				(count > 3)  ? printf("%s", LONG_ENDING) : printf("%s", SHORT_ENDING);
+				first_char = 0;
+			}
+
+			putchar(c);
+		}
+
+		first_char ? count++ : (count = 0);
+	}
+}
+
+int main(void)
+{
+	parse_input();
+
+	return 0;
+}
