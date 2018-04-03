9795dbf798d978f2bc2bd313278657628d111fe7
Jackson Petty <jacksonopetty@gmail.com>
2018-04-03 10:38:09 -0400

    Fix off-by-one error in sorting that caused trees of width 1 to never be sorted relative to their peers

--- Assignment 6/treeSort/treeSort/main.c
+++ Assignment 6/treeSort/treeSort/main.c
@@ -12,24 +12,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+void tests() {
+	char * rand100in = "[[[[[[[]]][][[][][[][]]]][[][]][[]][[[][][][]][[][]]][[[[[]][]][[[[][]][]]][][]][][[[][]]][]]][][[[[]][][]][]]][[][][[]]][[[[[[]][]][]][[]][[[]]][]][[[[]][]][[]][][[][[[]]]][]][[[][[[[]][][]][]][]]]]]";
+	char * rand100out = "[[[[[[[[[][]][]]][[[]][]][][]][[[][]]][][]][[[[][]][][]][[[]]][]][[[][][][]][[][]]][[][]][[]]][[[[]][][]][]][]][[[[[[]]][]][[[]][]][[]][][]][[[[[]][]][]][[[]]][[]][]][[[[[[]][][]][]][][]]]][[[]][][]]]";
+	
+	size_t length = strlen(rand100in);
+	Node * tree = parseTree(rand100in, 1, length - 1);
+	sortTree(tree);
+	printTree(tree);
+	fellTree(tree);
+	printf("\n#######################\n");
+	Node * oTree = parseTree(rand100out, 1, length - 1);
+	sortTree(oTree);
+	printTree(oTree);
+	fellTree(oTree);
+
+	
+}
+
 int main(int argc, const char * argv[]) {
+//	
+//	tests();
 	
 	if (argc != 1) {
 		fprintf(stderr, "Usage: %s", argv[0]);
 	}
-	
+
 	char * input = parseInput();
 
 	size_t i = strlen(input);
 
 	Node * tree = parseTree(input, 1, i - 1);
-	
+
 	sortTree(tree);
 	printTree(tree);
 	fellTree(tree);
 
 	free(input);
 	input = NULL;
-	
+
 	return EXIT_SUCCESS;
 }
--- Assignment 6/treeSort/treeSort/tree.c
+++ Assignment 6/treeSort/treeSort/tree.c
@@ -20,26 +20,26 @@
 // MARK: - Properties
 
 struct node {
+	size_t width;
 	size_t size;
-	size_t height;
 	Node * parent;
 	Node * children[];
 };
 
 // MARK: - Static Functions
 
-static Node * createNodeWithSize(size_t size) {
+static Node * createNodeWithChildren(size_t children) {
 	
-	Node * node = malloc(sizeof(*node) + size * sizeof(*node->children));
+	Node * node = malloc(sizeof(*node) + children * sizeof(*node->children));
 	if (NULL == node) {
 		fprintf(stderr, "Fatal Error: Unable to create node.\n");
 		exit(ERR_BAD_NODE);
 	}
 	
-	node->height = 1;
-	node->size = size;
+	node->size = 1;
+	node->width = children;
 	node->parent = NULL;
-	for (size_t i = 0; i < size; i++) {
+	for (size_t i = 0; i < children; i++) {
 		node->children[i] = NULL;
 	}
 	
@@ -52,11 +52,10 @@ static Node * createNodeWithSize(size_t size) {
  be replaced by a merge sort implementation.
 */
 
-static int compareTrees(const void * treeA, const void * treeB) {
+static inline int compareTrees(const void * treeA, const void * treeB) {
 	const Node * const leftTree = *(Node **)treeA;
 	const Node * const rightTree = *(Node **)treeB;
-	
-	return (int)(rightTree->height - leftTree->height);
+	return (int)(rightTree->size > leftTree->size);
 }
 
 // MARK: - Public Functions
@@ -68,16 +67,29 @@ static int compareTrees(const void * treeA, const void * treeB) {
  @param root	Pointer to the root node of a sub-tree.
 */
 void sortTree(Node * root) {
-	if (root->size > 1) {
+	
+	if (root == NULL) { return; }
+	
+	if (root->width > 0) {
 		
-		qsort(root->children, root->size, sizeof(Node *), compareTrees);
+		qsort(root->children, root->width, sizeof(Node *), compareTrees);
 		
-		for (size_t i = 0; i < root->size; i++) {
+		for (size_t i = 0; i < root->width; i++) {
 			sortTree(root->children[i]);
 		}
 	}
 }
 
+/**
+ Parse the input from stdin into a NULL terminated
+ buffer, which is then passed to the tree parser.
+ While parsing stdin, input is sanitized to remove
+ any illegal characters, or throw errors if input
+ is invalid. If a sring is returned from @c parseInput,
+ it is guaranteed to describe a valid tree.
+ 
+ @return The sanitized string of the tree.
+*/
 char * parseInput(void) {
 	
 	int c;
@@ -85,7 +97,7 @@ char * parseInput(void) {
 	size_t length = 0;
 	size_t size = BUFFER_SIZE;
 
-	char * buff = malloc(size * sizeof(char));
+	char * buffer = malloc(size * sizeof(char));
 	
 	while ((c = getchar()) != EOF) {
 		
@@ -97,70 +109,85 @@ char * parseInput(void) {
 			if (level == 0) {
 				break;
 			} else {
-				free(buff);
-				buff = NULL;
+				free(buffer);
+				buffer = NULL;
 				exit(ERR_BAD_INPUT);
 			}
 		}
 		
 		if (length >= (size-1)) {
 			size = ((length / BUFFER_SIZE) + 1) * BUFFER_SIZE;
-			buff = realloc(buff, size);
+			char * temp = realloc(buffer, size);
+			if (temp != NULL) {
+				buffer = temp;
+			} else {
+				fprintf(stderr, "Fatal Error: Could not reallocate space for buffer.\n");
+				exit(ERR_BAD_INPUT);
+			}
 		}
 		
-		buff[length++] = c;
+		buffer[length++] = c;
 	}
 	
-	buff[length] = '\0';
-	
 	if (length == 0) {
-		free(buff);
+		free(buffer);
 		exit(EXIT_SUCCESS);
 	}
 	
-	if (length % BUFFER_SIZE != 0) {
-		buff = realloc(buff, size - (BUFFER_SIZE % length));
-	}
+	buffer[length] = '\0';
 	
-	return buff;
+	return buffer;
 }
 
+/**
+ Parses a sanitized non-empty input string
+ into a single tree. Input must be a valid
+ sequence, or this will fail with a fatal
+ error.
+ 
+ @return The root node of the tree.
+*/
 Node * parseTree(const char * input, size_t start, size_t end) {
 	
-	char c;
-	size_t size = 0;	// How many children does a node have
-	size_t v_level = 0; // Vertical level of the tree
+	// TODO: Figure out why there is an off-by-one error
 	
-	for (size_t i = start ; i < end ; i++) {
+	size_t children = 0;	// How many children does a node have
+	size_t v_level = 0;     // Vertical level of the tree
+	
+	for (size_t i = start; i < end ; i++) {
 		
-		c = input[i];
-		('[' == c) ? v_level++ : v_level--;
-		if (v_level == 0) { size++; }
+		input[i] == '[' ? v_level++ : v_level--;
+		if (v_level == 0) { children++; }
 		
 	}
 	
-	Node * root = createNodeWithSize(size);
-	size_t locations[2];
+	Node * root = createNodeWithChildren(children);
+	size_t boundary[2];
 	
-	size = 0;
-	for (size_t i = start ; i < end ; i++) {
-		c = input[i];
+	children = 0;
+	for (size_t i = start; i < end ; i++) {
 		
-		if ('[' == c) {
-			if (v_level++ == 0) {
-				locations[0] = i;
-			}
-		} else {
-			if (--v_level == 0) {
-				locations[1] = i;
-				size++;
-			}
+		switch (input[i]) {
+			case '[':
+				if (v_level++ == 0) {
+					boundary[0] = i + 1;
+				}
+				break;
+			
+			case ']':
+				if (--v_level == 0) {
+					boundary[1] = i;
+					children++;
+				}
+				
+			default:
+				break;
 		}
 		
 		if (v_level == 0) {
-			root->children[size - 1] = parseTree(input, locations[0]+1, locations[1]);
-			root->children[size - 1]->parent = root;
-			root->height += root->children[size - 1]->height;
+			root->children[children - 1] = parseTree(input, boundary[0], boundary[1]);
+			root->children[children - 1]->parent = root;
+			root->size += root->children[children - 1]->size;
 		}
 		
 	}
@@ -170,11 +197,13 @@ Node * parseTree(const char * input, size_t start, size_t end) {
 
 void printTree(Node * tree) {
 	
-//	printf("\nNode has size of %d\n", tree->height);
+	if (tree == NULL) { return; }
+	
+//	printf("\nNode has size of %d\n", tree->size);
 	
 	printf("[");
 	
-	for (size_t i = 0; i < tree->size; i++) {
+	for (size_t i = 0; i < tree->width; i++) {
 		printTree(tree->children[i]);
 	}
 	
@@ -183,7 +212,9 @@ void printTree(Node * tree) {
 
 void fellTree(Node * tree) {
 	
-	for (size_t i = 0; i < tree->size; i++) {
+	if (tree == NULL) { return; }
+	
+	for (size_t i = 0; i < tree->width; i++) {
 		fellTree(tree->children[i]);
 	}
 
